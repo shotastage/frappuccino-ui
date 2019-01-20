741c22d26cb0201bcd76f1e7587ceb05832e0200
shotastage <hornet.live.mf@gmail.com>
2019-01-21 01:53:00 +0900

    Initial Commit

---
+++ Frappuccino/Frappuccino.h
@@ -0,0 +1,19 @@
+//
+//  Frappuccino.h
+//  Frappuccino
+//
+//  Created by Shota Shimazu on 2019/01/21.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Frappuccino.
+FOUNDATION_EXPORT double FrappuccinoVersionNumber;
+
+//! Project version string for Frappuccino.
+FOUNDATION_EXPORT const unsigned char FrappuccinoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frappuccino/PublicHeader.h>
+
+

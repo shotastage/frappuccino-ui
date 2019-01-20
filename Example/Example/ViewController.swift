dc1dab388ee1bf91afa579c270d1204705f936a2
shotastage <hornet.live.mf@gmail.com>
2019-01-21 02:10:07 +0900

    first commit

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Shota Shimazu on 2019/01/21.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Frappuccino/Frappuccino.swift
@@ -0,0 +1,13 @@
+//
+//  Frappuccino.swift
+//  Frappuccino
+//
+//  Created by Shota Shimazu on 2019/01/21.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import Foundation
+
+open class frappuccino {
+    static let version = "0.0.1"
+}

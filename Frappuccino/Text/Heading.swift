0a23d7f39641605884d87a7be3b5b34fef45440a
Shota Shimazu <hornet.live.mf@gmail.com>
2019-02-12 22:46:38 +0900

    add heading property

--- Frappuccino/Text/Heading.swift
+++ Frappuccino/Text/Heading.swift
@@ -10,29 +10,40 @@ import UIKit
 
 
 
-open class Header: UILabel {
+open class Heading: UILabel {
     
     public var fontName: String = "Helvetica-Bold"
     
     private var fontTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Helvetica-Bold", size: 22)!]
     
     override public init(frame: CGRect) {
-        super.init(frame: CGRect(x: frame.origin.x, y: frame.origin.y, width: Size.width, height: Size.height))
-        
-        self.fontTextAttributes = [NSAttributedString.Key.font: UIFont(name: self.fontName, size: 22)!]
+        super.init(frame: frame)
     }
     
-    convenience init(text: String, size: Header.HeadingSize = .one) {
+    convenience init(text: String, size: Heading.Headings = .h1) {
         self.init()
+        
+        self.frame = CGRect(
+            x: frame.origin.x,
+            y: frame.origin.y,
+            width: Size.width,
+            height: font.lineHeight
+        )
+        
+        self.fontTextAttributes = [NSAttributedString.Key.font: UIFont(name: self.fontName, size: 22)!]
+
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public enum HeadingSize {
-        case one
-        case two
-        case three
+    public enum Headings {
+        case h1
+        case h2
+        case h3
+        case h4
+        case h5
+        case h6
     }
 }

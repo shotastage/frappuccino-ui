ba776092c7c5e55af23a2dd9a353c972df6ba1c5
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-21 23:39:06 +0900

    add tableview controller

---
+++ Whipcream/TableView/TableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TableViewController.swift
+//  Whipcream
+//
+//  Created by Shota Shimazu on 2019/04/21.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+
+
+open class TableView: UITableView {
+    
+    var isNoneSeparator = false
+    
+    override public init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        
+        self.separatorInset = UIEdgeInsets.zero
+        
+        if isNoneSeparator {
+            self.separatorStyle = .none
+        }
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

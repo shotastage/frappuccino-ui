972c9c0460cf979ae5ba6e4b4ce8e8de832601e5
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-22 21:35:34 +0900

    add control state

---
+++ Whipcream/Whipcream+Control.swift
@@ -0,0 +1,25 @@
+//
+//  Whipcream+Control.swift
+//  Whipcream
+//
+//  Created by Shota Shimazu on 2019/04/22.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import Foundation
+
+
+public enum OnOff {
+    case on
+    case off
+    
+    var boolean: Bool {
+        switch self {
+        case .on:
+            return true
+        case .off:
+            return false
+        }
+    }
+}
+

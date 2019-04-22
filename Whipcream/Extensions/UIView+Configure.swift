2293f1bd06710911fa834fdd6073886424521c43
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-22 21:36:02 +0900

    revise

---
+++ Whipcream/Extensions/UIView+Configure.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Configure.swift
+//  Whipcream
+//
+//  Created by Shota Shimazu on 2019/04/22.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIView {
+    
+}

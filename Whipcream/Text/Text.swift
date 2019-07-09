bb80e635808a817df2649fa948788c89c852f5e7
Shota Shimazu <hornet.live.mf@gmail.com>
2019-07-09 14:00:58 +0900

    revise

--- Whipcream/Text/Text.swift
+++ Whipcream/Text/Text.swift
@@ -1,8 +1,8 @@
 //
 //  Text.swift
-//  Frappuccino
+//  Whipcream
 //
-//  Created by Shota Shimazu on 2019/01/27.
+//  Created by Shota Shimazu on 2019/07/09.
 //  Copyright © 2019 Shota Shimazu. All rights reserved.
 //
 
@@ -10,26 +10,60 @@ import UIKit
 
 
 
-@IBDesignable
-open class Text: UIView {
+open class Text: UILabel {
 
-    public var text: String? // default is nil
+    /// Font propertoes
+    public var fontName: String = "Helvetica-Bold"
 
-    public var font: UIFont! // default is nil (system font 17 plain)
+    public var fontSize: CGFloat = 22.0
 
-    public var textColor: UIColor! // default is nil (text draws black)
+    public var color: UIColor = UIColor(hex: "333333")
 
-    public var textAlignment: NSTextAlignment!
+    /// Layout properties
+    public var margin: Margin = Margin()
 
-    public var attributedText: NSAttributedString? // default is nil
 
-
-    convenience public init(text: String, size: Text.TextSize = .bold) {
-        self.init()
+    private var fontTextAttributes: [NSAttributedString.Key: UIFont] {
+        get {
+            return [NSAttributedString.Key.font: UIFont(name: self.fontName, size: self.fontSize)!]
+        }
     }
 
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
 
-    public enum TextSize {
-        case bold
+    public convenience init(text: String, size: CGFloat) {
+        self.init()
+
+        self.text = text
+        self.numberOfLines = 1
+        self.font = UIFont(name: fontName, size: size)
+        self.frame.size.width = Size.width - MarginSize.small
+        self.textAlignment = .left
+        self.textColor = self.color
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override open func drawText(in rect: CGRect) {
+        let newRect = rect.inset(by: margin.view)
+
+        super.drawText(in: newRect)
+    }
+
+    public enum Headings: CGFloat {
+        case h1 = 35.0
+        case h2 = 28.0
+        case h3 = 23.0
+        case h4 = 18.0
+        case h5 = 15.0
+        case h6 = 12.0
+    }
+
+    public static func free(size: CGFloat) -> Headings {
+        return Headings(rawValue: size)!
     }
 }
